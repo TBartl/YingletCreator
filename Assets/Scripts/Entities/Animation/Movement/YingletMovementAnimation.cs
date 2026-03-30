3208d831bd46e1498973c81be7635d722663c562
Thomas Bartlett <tbartl@umich.edu>
2026-03-30 16:33:03 -0400

    Walk animation

--- Assets/Scripts/Entities/Animation/Movement/YingletMovementAnimation.cs
+++ Assets/Scripts/Entities/Animation/Movement/YingletMovementAnimation.cs
@@ -7,11 +7,18 @@ using UnityEngine;
 public class YingletMovementAnimation : MonoBehaviour
 {
 	[SerializeField] float SPEED_THRESHOLD = 0.1f;
+	[SerializeField] float WALKING_ANIM_SPEED = 1f;
+
 	static string[] IDLE_LAYER_NAMES = new string[] { "TailWagging", "LookAround", "EarWiggle" };
+	static string WALKING_LAYER_NAME = "Walking";
+	static string MOVE_CYCLE_SPEED_PARAM_NAME = "MoveCycleSpeed";
 
 	private Rigidbody _rigidBody;
 	private Animator _animator;
-	private IEnumerable<YingLayer> _layers;
+
+	private IEnumerable<YingLayer> _idleLayers;
+	private YingLayer _walkingLayer;
+	private int _moveCycleSpeedParam;
 
 
 
@@ -20,7 +27,9 @@ public class YingletMovementAnimation : MonoBehaviour
 	{
 		_rigidBody = this.GetComponentInParent<Rigidbody>();
 		_animator = this.GetComponent<Animator>();
-		_layers = IDLE_LAYER_NAMES.Select(layerName => new YingLayer(layerName, _animator)).ToArray();
+		_idleLayers = IDLE_LAYER_NAMES.Select(layerName => new YingLayer(layerName, _animator)).ToArray();
+		_walkingLayer = new YingLayer(WALKING_LAYER_NAME, _animator);
+		_moveCycleSpeedParam = Animator.StringToHash(MOVE_CYCLE_SPEED_PARAM_NAME);
 	}
 
 	// Update is called once per frame
@@ -29,9 +38,14 @@ public class YingletMovementAnimation : MonoBehaviour
 		float speed = _rigidBody.linearVelocity.magnitude;
 		bool moving = speed > SPEED_THRESHOLD;
 
-		foreach (var layer in _layers)
+		foreach (var layer in _idleLayers)
 		{
 			_animator.SetLayerWeight(layer.LayerIndex, moving ? 0 : layer.OriginalWeight);
+		}
+		_animator.SetLayerWeight(_walkingLayer.LayerIndex, moving ? _walkingLayer.OriginalWeight : 0);
+		if (moving)
+		{
+			_animator.SetFloat(_moveCycleSpeedParam, speed * WALKING_ANIM_SPEED);
 		}
 	}
 
