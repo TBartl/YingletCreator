50eeb989d00fa0f20223dec27fbd85d93aa30492
Thomas Bartlett <tbartl@umich.edu>
2026-04-12 00:38:41 -0400

    Add confirmation on exit

--- Assets/Scripts/Entities/Character/Creator/LeaveCharacterCreatorOnEscPressed.cs
+++ Assets/Scripts/Entities/Character/Creator/LeaveCharacterCreatorOnEscPressed.cs
@@ -1,23 +1,41 @@
+using Character.Creator;
 using UnityEngine;
 
 public class LeaveCharacterCreatorOnEscPressed : MonoBehaviour
 {
+	private ICustomizationSelection _selection;
 	private ICharacterCreatorTracker _characterCreatorTracker;
 	private IMenuManager _menuManager;
+	private IConfirmationManager _confirmationManager;
 
 	private void Awake()
 	{
+		_selection = Singletons.GetSingleton<ICustomizationSelection>();
 		_characterCreatorTracker = Singletons.GetSingleton<ICharacterCreatorTracker>();
 		_menuManager = Singletons.GetSingleton<IMenuManager>();
+		_confirmationManager = Singletons.GetSingleton<IConfirmationManager>();
 	}
 
 	void Update()
 	{
 		if (!_characterCreatorTracker.IsInCharacterCreator.Val) return;
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (!Input.GetKeyDown(KeyCode.Escape)) return;
+
+		if (_selection.SelectionIsDirty)
 		{
-			// TODO: Intercept if anything is changed and push menu
-			// Also check if that's open
+			_confirmationManager.OpenConfirmation(new(
+				"You have unsaved changes\n\nAre you sure you want to exit the character creator?",
+				"Exit Anyway",
+				"change-yinglet-selection",
+				PopMenu));
+		}
+		else
+		{
+			PopMenu();
+		}
+
+		void PopMenu()
+		{
 			_menuManager.PopMenu();
 		}
 	}
