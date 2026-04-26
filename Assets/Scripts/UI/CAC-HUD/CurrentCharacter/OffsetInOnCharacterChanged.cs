a42b461087a6a11c582c019199a152158d25b86d
Thomas Bartlett <tbartl@umich.edu>
2026-04-26 02:57:38 -0400

    Offset portrait in

---
+++ Assets/Scripts/UI/CAC-HUD/CurrentCharacter/OffsetInOnCharacterChanged.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class OffsetInOnCharacterChanged : MonoBehaviour
+{
+	[SerializeField] SharedEaseSettings _easeSettings;
+	[SerializeField] Vector2 _offset;
+
+	private Coroutine _transitionCoroutine;
+	private IPartyMemberHUDReference _reference;
+	private RectTransform _rectTransform;
+	private Vector2 _originalAnchoredPosition;
+
+	private void Start()
+	{
+		_rectTransform = this.GetComponent<RectTransform>();
+		_originalAnchoredPosition = _rectTransform.anchoredPosition;
+		_reference = this.GetComponentInParentSafe<IPartyMemberHUDReference>();
+		_reference.CharacterGameObjectObservable.OnChanged += CharacterGameObject_OnChanged;
+	}
+	private void OnDestroy()
+	{
+		if (_reference.CharacterGameObjectObservable != null)
+		{
+			_reference.CharacterGameObjectObservable.OnChanged -= CharacterGameObject_OnChanged;
+		}
+	}
+
+	private void CharacterGameObject_OnChanged(GameObject from, GameObject to)
+	{
+		Vector2 fromPos = _rectTransform.anchoredPosition + _offset;
+		Vector2 toPos = _originalAnchoredPosition;
+		this.StartEaseCoroutine(ref _transitionCoroutine, _easeSettings, p => _rectTransform.anchoredPosition = Vector2.LerpUnclamped(fromPos, toPos, p));
+	}
+}
