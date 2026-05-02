7dc2460b5972bb92614fd46ecd3d8d41f1c55c7e
Thomas Bartlett <tbartl@umich.edu>
2026-05-02 02:36:51 -0400

    Fix frame encounter camera jump

--- Assets/Scripts/Gameplay/Camera/CameraControlProvider_FrameEncounter.cs
+++ Assets/Scripts/Gameplay/Camera/CameraControlProvider_FrameEncounter.cs
@@ -7,20 +7,27 @@ internal class CameraControlProvider_FrameEncounter : ReactiveBehaviour, ICamera
 {
 	private IActiveEncounterProvider _activeEncounterProvider;
 	private Computed<Transform> _encounterTransform;
+	private Vector3 _pos;
+	private Quaternion _rot;
 
 	public bool WantsControl => _encounterTransform.Val != null;
 
 	public (Vector3, Quaternion) CalculateTransform()
 	{
 		var encounterTransform = _encounterTransform.Val;
-		if (encounterTransform == null) return (Vector3.zero, Quaternion.identity);
-		return (encounterTransform.position, encounterTransform.rotation);
+		if (encounterTransform != null)
+		{
+			_pos = encounterTransform.position;
+			_rot = encounterTransform.rotation;
+		}
+		return (_pos, _rot);
 	}
 
 	public void Initialize()
 	{
 		_activeEncounterProvider = Singletons.GetSingleton<IActiveEncounterProvider>();
 		_encounterTransform = CreateComputed(ComputeEncounterTransform);
+
 	}
 
 	private Transform ComputeEncounterTransform()
