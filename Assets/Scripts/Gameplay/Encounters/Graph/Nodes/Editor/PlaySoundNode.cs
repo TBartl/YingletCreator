1a742e81141a46a2422799b00617aa3c5681b61a
Thomas Bartlett <tbartl@umich.edu>
2026-05-04 01:06:34 -0400

    Sound effects

---
+++ Assets/Scripts/Gameplay/Encounters/Graph/Nodes/Editor/PlaySoundNode.cs
@@ -0,0 +1,39 @@
+﻿using Encounters.Runtime;
+using System;
+using Unity.GraphToolkit.Editor;
+
+namespace Encounters.Editor
+{
+	[Serializable]
+	public class PlaySoundNode : Node, IEditorNode
+	{
+		const string SOUND_EFFECT_PORT_NAME = "Sound Effect";
+		const string PLAY_LOCATION_PORT_NAME = "Play Location";
+
+		protected override void OnDefinePorts(IPortDefinitionContext context)
+		{
+			context.AddInputPort(EditorNodeUtils.EXECUTION_PORT_NAME)
+				.WithDisplayName(string.Empty)
+				.WithConnectorUI(PortConnectorUI.Arrowhead)
+				.Build();
+
+			context.AddInputPort<SoundEffect>(SOUND_EFFECT_PORT_NAME)
+				.Build();
+
+			context.AddInputPort<EncounterPlaySoundLocation>(PLAY_LOCATION_PORT_NAME)
+				.Build();
+
+			context.AddOutputPort(EditorNodeUtils.EXECUTION_PORT_NAME)
+				.WithDisplayName(string.Empty)
+				.WithConnectorUI(PortConnectorUI.Arrowhead)
+				.Build();
+		}
+
+		public IEncounterNode CreateRuntimeNode()
+		{
+			SoundEffect soundEffect = this.GetPortValue<SoundEffect>(SOUND_EFFECT_PORT_NAME);
+			EncounterPlaySoundLocation playLocation = this.GetPortValue<EncounterPlaySoundLocation>(PLAY_LOCATION_PORT_NAME);
+			return new Runtime.PlaySoundNode(soundEffect, playLocation);
+		}
+	}
+}
---
+++ Assets/Scripts/Gameplay/Encounters/Graph/Nodes/PlaySoundNode.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Encounters.Runtime
+{
+	public enum EncounterPlaySoundLocation
+	{
+		EncounterSource,
+		Character
+	}
+
+	[System.Serializable]
+	public sealed class PlaySoundNode : SingleOutputNode
+	{
+		[SerializeField] private SoundEffect _soundEffect;
+		[SerializeField] private EncounterPlaySoundLocation _playLocation;
+
+		public PlaySoundNode(SoundEffect soundEffect, EncounterPlaySoundLocation playLocation)
+		{
+			_soundEffect = soundEffect;
+			_playLocation = playLocation;
+		}
+
+		public override void Run(IEncounterInstance encounterInstance)
+		{
+			encounterInstance.ProgressToNode(_next);
+			PlaySound(encounterInstance);
+		}
+
+		void PlaySound(IEncounterInstance encounterInstance)
+		{
+			var audioPlayer = Singletons.GetSingleton<IAudioPlayer>();
+			var position = _playLocation switch
+			{
+				EncounterPlaySoundLocation.EncounterSource => encounterInstance.EncounterSource.transform.position,
+				EncounterPlaySoundLocation.Character => encounterInstance.Character.transform.position,
+				_ => throw new System.NotImplementedException($"Unknown {nameof(EncounterPlaySoundLocation)}: {_playLocation}")
+			};
+			audioPlayer.Play(_soundEffect, new AudioPlayOptions() { Position = position });
+		}
+	}
+}
