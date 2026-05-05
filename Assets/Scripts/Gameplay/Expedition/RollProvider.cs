fe8c4ea861814ae8128117ec2bb1bfb012720458
Thomas Bartlett <tbartl@umich.edu>
2026-05-05 02:57:00 -0400

    Roll forcing

--- Assets/Scripts/Gameplay/Expedition/RollProvider.cs
+++ Assets/Scripts/Gameplay/Expedition/RollProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Encounters;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public interface IRollProvider
@@ -10,12 +12,24 @@ internal class RollProvider : MonoBehaviour, IRollProvider, IInitializable
 {
 	private IDeterministicRandomProvider _random;
 
+	Queue<int> _forcedRolls = new Queue<int>();
+
 	public void Initialize()
 	{
 		_random = this.GetComponentSafe<IDeterministicRandomProvider>();
+
+		_forcedRolls.Enqueue(2);
+		_forcedRolls.Enqueue(5);
 	}
 	public int GetRoll(ICharacterRoot character, RollType rollType)
 	{
+		if (_forcedRolls.Any())
+		{
+			var result = _forcedRolls.Dequeue();
+			Debug.LogWarning($"Using forced roll of {result}. These shouldn't be set outside of recording purposes.");
+			return result;
+		}
+
 		int sum = 0;
 
 		// Basic betrayal at house on the hill like rolling assuming stat of 4
