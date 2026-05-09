32f99b87d7ebd30d433e65b9eeb8bc02d9c2b6de
Thomas Bartlett <tbartl@umich.edu>
2026-05-09 03:06:18 -0400

    Play sound on round changed

---
+++ Assets/Scripts/Gameplay/Expedition/PlaySoundOnRoundChanged.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlaySoundOnRoundChanged : MonoBehaviour
+{
+	[SerializeField] private SoundEffect _soundEffect;
+	private AudioPlayer _audioPlayer;
+	private IExpeditionRoundManager _roundManager;
+
+	void Start()
+	{
+		_audioPlayer = Singletons.GetSingleton<AudioPlayer>();
+		_roundManager = this.GetExpeditionComponent<IExpeditionRoundManager>();
+
+		_roundManager.CurrentRound.OnChanged += Round_OnChanged;
+	}
+
+	private void OnDestroy()
+	{
+		if (_roundManager != null)
+		{
+			_roundManager.CurrentRound.OnChanged -= Round_OnChanged;
+		}
+	}
+
+	private void Round_OnChanged(int from, int to)
+	{
+		_audioPlayer.Play(_soundEffect);
+	}
+}
